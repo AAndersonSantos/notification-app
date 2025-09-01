fffab7aaaf2fe90a50c5bdc4c51487747a8d7e75
AAndersonSantos <andersonsantos.inc@gmail.com>
2025-09-01 00:28:58 -0300

    FEAT: Configuração do cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/notificationRoutes";
 import { connectDatabase } from "./config/database";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
